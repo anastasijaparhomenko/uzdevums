1fccaf3ad67efc7c11a504062e291165c1d0eb25
anastasijaparhomenko <56106798+anastasijaparhomenko@users.noreply.github.com>
2020-11-30 11:15:36 +0200

    Add files via upload

---
+++ novembris-main/kludas/not_found.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="icon" href="https://i.dlpng.com/static/png/4674218_preview.png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MineCurse- Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="./css/reset.css" rel="stylesheet">
+    <link href = "./libs/bootstrap/css/bootstrap.css" rel ="stylesheet">
+    <script src="./libs/jQuery/jquery3.4.1.min.js"></script>
+    <script src="./libs/bootstrap/js/bootstrap.js"> </script>
+    <link href="./css/style.css" rel="stylesheet">
+    <link href="./css/lightbox.css" rel="stylesheet"/>
+    <script src="./libs/lightbox/js/lightbox.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="parmums.php">Covid Drošība</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav  navbar-right">
+                <li class="active"><a href="index">Mājas</a></li>
+                <li><a href="parmums">Par mums</a></li>
+                <li><a href="galerija">Galerija</a></li>
+                <li><a href="videogalerija">Video galerija</a></li>
+                <li><a href="kontakti">Kontakti</a></li>
+            </ul>
+
+        </div>
+    </div>
+</nav>
+<section class="container-fluid">
+    <section class="row">
+        <div class="col-md-2 col-sm-6 article" style="background-color:#FFFFFF">
+            <h1>LAPA NAV ATRASTA!</h1>
+        </div>
+        <div class="col-md-8 col-sm-6 article" style="background-color:#FFFFFF">
+            <h1>Covid Drošība</h1>
+            <h3>LAPA NETIKA ATRASTA!</h3>
+	    <img src="./images/citiatt/kluda404" alt="Mājaslapa nestrādā, attēls arī">
+	    <p></p>
+	    <a href="index" class="btn btn-success myBtn" role="button">Atgriezties uz mājām</a>
+            <p></p>
+        </div>
+        <div class="col-md-2 col-sm-6 article" style="background-color:#FFFFFF">
+            <h1>LAPA NAV ATRASTA!</h1>
+        </div>
+    </section>
+</section>
+</body>
+<div class="footer">
+    <p>Kājene</p>
+    <div class="footerimgbox">
+        <a href="https://www.w3schools.com">
+            <img alt="Instagram.com logo" height="60px" width="60px" src="https://i.pinimg.com/originals/4f/01/49/4f0149647a160a47217615866f5469c4.png">
+        </a>
+        <a href="https://www.rvt.lv/">
+            <img alt="Rvt.lv logo" height="60px" width="60px" src=https://www.estudijas.rvt.lv/theme/image.php/clean/theme/1505375536/rvt-95">
+        </a>
+        <a href="https://www.facebook.com">
+            <img alt="Facebook.com logo" height="60px" width="60px" src="https://www.freepnglogos.com/uploads/facebook-icons/facebook-icon-transparent-background-3.png">
+        </a>
+        <a href="https://www.twitter.com">
+            <img alt="Twitter.com logo" height="60px" width="60px" src="https://aer.eu/wp-content/uploads/2017/06/twitter-logo-4.png">
+        </a>
+        <a href="https://w3schools.com">
+            <img alt="W3schools.com logo" height="60px" width="60px" src="https://www.w3schools.com/tags/logo_w3s.gif">
+        </a>
+    </div>
+    <a href="">
+        <p>Copyright©-MineCurse-2019.12.x</p>
+        <p>Simbolisks teksts lai izskatītos pēc īstās dzīves piemēra.</p>
+    </a>
+</div>
+</html>
